6f7f9b1c0c84ae1564e7e82a1831ade6e34929eb
Alexandre Girard <girarda.92@gmail.com>
2015-07-26 23:40:00 -0700

    Implement char-to-int.

--- src/urlshortener/core.clj
+++ src/urlshortener/core.clj
@@ -19,5 +19,18 @@
     (and (>= i 36) (< i 62)) (+ i LOWERCASE_OFFSET)
     :else (throw Exception))))
 
+(defn is-digit? [c]
+  (let [zero (int \0)
+        nine (int \9)]
+    (<= zero (int c ) nine)))
+
+(defn char-to-int [c]
+  (let [i (int c)]
+    (cond
+    (is-digit? c) (-  i DIGIT_OFFSET)
+    (and (>= i (int \A)) (<= i (int \Z))) (- i UPPERCASE_OFFSET)
+    (and (>= i (int \a)) (<= i (int \z))) (- i LOWERCASE_OFFSET)
+    :else (throw Exception))))
+
 (defn int-to-base62 [x]
   (clojure.string/join "" (map int-to-char (int-to-mod-list x 62))))
--- test/urlshortener/core_test.clj
+++ test/urlshortener/core_test.clj
@@ -34,3 +34,23 @@
   (testing "converts int to base62 string"
     (is (= "21"
            (int-to-base62 125)))))
+
+(deftest test-char-to-int
+  (testing "single digit returns digit"
+    (is (and (= 0
+                (char-to-int \0))
+             (= 9
+                (char-to-int \9)))))
+  (testing "between 10 and 35 return uppercase char"
+        (is (and (= 10
+                (char-to-int \A))
+             (= 35
+                (char-to-int \Z)))))
+  (testing "between 36 and 61 return lowercase char"
+        (is (and (= 36
+                (char-to-int \a))
+             (= 61
+                (char-to-int \z)))))
+  (testing "higher than 61 throws exception"
+        (is (thrown? Exception
+               (char-to-int \tab)))))
