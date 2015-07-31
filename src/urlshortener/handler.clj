31672a3136c08376909ec77a0329692da20cdbdb
Alexandre Girard <girarda.92@gmail.com>
2015-07-30 22:58:27 -0700

    Retrieve url from short url in browser

--- src/urlshortener/handler.clj
+++ src/urlshortener/handler.clj
@@ -1,10 +1,19 @@
 (ns urlshortener.handler
-  (:require [compojure.core :refer :all]
+  (:require [urlshortener.core :refer :all]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn handle-request [short-url]
+  (let [url (retrieve-url short-url)]
+    (if (nil? url)
+      (route/not-found "Not Found")
+      {:status 302
+        :headers {"Location" (str "http://" url)}})))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/:short-url" [short-url] (handle-request short-url))
   (route/not-found "Not Found"))
 
 (def app
