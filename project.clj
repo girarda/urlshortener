8c6c24a660ee1793cbe1188a8f07cd4645220d26
Alexandre Girard <girarda.92@gmail.com>
2015-07-30 22:53:57 -0700

    Integreate compojure

--- project.clj
+++ project.clj
@@ -3,9 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :main urlshortener.core
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [com.taoensso/carmine "2.11.1"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.7.0"]]}})
+                 [com.taoensso/carmine "2.11.1"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler urlshortener.handler/app}
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.7.0"]
+                             [javax.servlet/servlet-api "2.5"]
+                             [ring-mock "0.1.5"]]}})
